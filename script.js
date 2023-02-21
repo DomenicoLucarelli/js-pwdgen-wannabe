86215e245ea1883871f40447c525b61b768c5aab
DomenicoLucarelli <domyluca@gmail.com>
2023-02-21 14:53:44 +0100

    esercizio completato

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+-chiedi nome all'utente e memorizzalo
+-chiedi cognome all'utente e memorizzalo
+-chiedi colore prefe e memorizzalo
+-visualizza a schermo con un numero scelto a caso da me
+*/
+
+let nomeUtente = prompt("inserisci il tuo nome");
+
+let cognomeUtente = prompt("inserisci il tuo cognome");
+
+let coloreUtente = prompt("inserisci il tuo colore preferito");
+
+// document.writeln(nomeUtente + cognomeUtente + coloreUtente + "26");
+
+document.writeln(`${nomeUtente}${cognomeUtente}${coloreUtente}"26"`);
+
+
+
